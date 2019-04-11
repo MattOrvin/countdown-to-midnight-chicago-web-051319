bd11f689a47d6a268c2aa23814da8867d865e9fa
Matt Orvin <matthew.orvin@gmail.com>
2019-04-11 22:01:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,8 +7,8 @@ def countdown(number)
   while number > 0 
   puts "#{number} SECOND(S)!"
   number -=1
-  countdown_with_sleep
-    puts "HAPPY NEW YEAR!"
+countdown_with_sleep
+  puts "HAPPY NEW YEAR!"
 end
 end
 
